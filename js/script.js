2b7cdfcfe85aae6930d481e6828f923deb26d265
sheben <sheben404@gmail.com>
2020-11-20 06:58:35 +0800

    add backgroundImage

--- js/script.js
+++ js/script.js
@@ -3,11 +3,22 @@ const context = chess.getContext('2d')
 
 context.strokeStyle = '#BFBFBF'
 
-for(let i= 0; i < 15; i++){
-  context.moveTo(15 + i*30,15)
-  context.lineTo(15 + i*30,435)
-  context.stroke()
-  context.moveTo(15 ,15+ i*30)
-  context.lineTo(435,15+ i*30)
-  context.stroke()
+const logo = new Image()
+logo.src = './images/logo.png'
+logo.onload = function (){
+  context.drawImage(logo,0,0,450,450)
+  drawChessBoard()
 }
+
+
+function drawChessBoard(){
+  for(let i= 0; i < 15; i++){
+    context.moveTo(15 + i*30,15)
+    context.lineTo(15 + i*30,435)
+    context.stroke()
+    context.moveTo(15 ,15+ i*30)
+    context.lineTo(435,15+ i*30)
+    context.stroke()
+  }
+}
+
