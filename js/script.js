c37c31287a0619f7ef362001793d6621c8dc3161
sheben <sheben404@gmail.com>
2020-11-20 14:35:01 +0800

    实现下棋步骤

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const chess = document.getElementById('chess')
 const context = chess.getContext('2d')
+let me = true
+let chessBoard = []
+
+for(let i = 0;i<15;i++){
+  chessBoard[i] = []
+  for(let j = 0;j<15;j++){
+    chessBoard[i][j] = 0
+  }
+}
 
 context.strokeStyle = '#BFBFBF'
 
@@ -22,3 +31,31 @@ function drawChessBoard(){
   }
 }
 
+const oneStep = function (i,j,me){
+  context.beginPath()
+  context.arc(15 + i*30,15+j*30,13,0,2*Math.PI)
+  context.closePath()
+  const gradient = context.createRadialGradient(15 + i*30, 15 + j*30-2, 13,15 + i*30+2, 15 + j*30-2,0)
+  if(me){
+    gradient.addColorStop(0,"#0A0A0A")
+    gradient.addColorStop(1,"#636766")
+  }else{
+    gradient.addColorStop(0,"#D1D1D1")
+    gradient.addColorStop(1,"#F9F9F9")
+  }
+  context.fillStyle = gradient
+  context.fill()
+}
+
+chess.onclick = function (e){
+  const x = e.offsetX
+  const y = e.offsetY
+  const i = Math.floor(x/30)
+  const j = Math.floor(y/30)
+  if(chessBoard[i][j] === 0){
+    chessBoard[i][j] = 1
+    oneStep(i,j,me)
+    me = !me
+  }
+
+}
